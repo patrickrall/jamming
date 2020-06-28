8f5c943a420f5330372ebecebe98ca7008041aec
Siyuan Stella Wang <siyuanswang@utexas.edu>
2020-06-28 14:29:44 -0500

    tsx file

---
+++ tiled_assets/dnatiles.tsx
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="dnatiles" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="dnatiles.png" width="160" height="160"/>
+ <wangsets>
+  <wangset name="dna" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#2ac2f8" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#ee00ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0x3030103"/>
+   <wangtile tileid="1" wangid="0x1030303"/>
+   <wangtile tileid="2" wangid="0x3010303"/>
+   <wangtile tileid="3" wangid="0x3030301"/>
+   <wangtile tileid="4" wangid="0x3010103"/>
+   <wangtile tileid="5" wangid="0x1030103"/>
+   <wangtile tileid="6" wangid="0x3030101"/>
+   <wangtile tileid="7" wangid="0x1010303"/>
+   <wangtile tileid="8" wangid="0x3010301"/>
+   <wangtile tileid="9" wangid="0x1030301"/>
+   <wangtile tileid="10" wangid="0x1010103"/>
+   <wangtile tileid="11" wangid="0x1010301"/>
+   <wangtile tileid="12" wangid="0x1030101"/>
+   <wangtile tileid="13" wangid="0x3010101"/>
+   <wangtile tileid="14" wangid="0x1010101"/>
+   <wangtile tileid="15" wangid="0x3030303"/>
+   <wangtile tileid="16" wangid="0x3030303"/>
+   <wangtile tileid="20" wangid="0x3030302"/>
+   <wangtile tileid="21" wangid="0x3030203"/>
+   <wangtile tileid="22" wangid="0x3020303"/>
+   <wangtile tileid="23" wangid="0x2030303"/>
+   <wangtile tileid="24" wangid="0x3030202"/>
+   <wangtile tileid="25" wangid="0x3020302"/>
+   <wangtile tileid="26" wangid="0x2030302"/>
+   <wangtile tileid="30" wangid="0x3030102"/>
+   <wangtile tileid="31" wangid="0x3010302"/>
+   <wangtile tileid="32" wangid="0x1030302"/>
+   <wangtile tileid="33" wangid="0x3010203"/>
+   <wangtile tileid="34" wangid="0x1030203"/>
+   <wangtile tileid="35" wangid="0x3030201"/>
+   <wangtile tileid="36" wangid="0x1020303"/>
+   <wangtile tileid="37" wangid="0x3020301"/>
+   <wangtile tileid="38" wangid="0x3020103"/>
+   <wangtile tileid="40" wangid="0x2030301"/>
+   <wangtile tileid="41" wangid="0x2030103"/>
+   <wangtile tileid="42" wangid="0x2010303"/>
+  </wangset>
+ </wangsets>
+</tileset>
