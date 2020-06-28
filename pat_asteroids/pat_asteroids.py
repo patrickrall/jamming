a2fc38d4568a8e6da55062ec7e9f2d00609e1bfc
Patrick Rall <patrickrall.ddns.net>
2020-06-28 13:29:37 -0500

    animated pat_asteroids, made spritesheet editor take command line args

--- pat_asteroids/pat_asteroids.py
+++ pat_asteroids/pat_asteroids.py
@@ -8,23 +8,138 @@ from swyne.images import load_spritesheet
 
 import random
 
-@listener
-def draw(w):
 
+# TODO:
+# add support for animation
+# make menu screen using node graph
+
+
+
+def run_game():
+
+    w = NodeWindow()
+    w.fps = 30 # yield "on_draw" and "on_frame" events 30 times per second
+
+    # global variables for gameplay
+    global player
+    player = {"x":100, "y":100, "alive":True, "respawn_timer": 0, "lives":3}
+
+    # initialize the player's animation information
+    global player_display
+    player_display = {"should_draw": True, "key":"player", "frame":0, "frame_clock":0}
+
+    global asteroids
+    asteroids = [{"x":50, "y":150, "vx":100, "vy":100}]
+
+
+    # node graph for menu system
+    w.node, keys = deserialize_node("""
+    hint HintedLayoutNode
+    -PaddingLayoutNode [5,"*","*","*"]
+    --label LabelNode "{color (255,255,255,255)} placeholder"
+    """)
+
+    # global variables for menu system
+    global window_dims
+    window_dims = {"width": 800, "height":600}
+    keys["hint"].mindims.x = window_dims["width"]
+    keys["hint"].maxdims.x = window_dims["width"]
+    keys["hint"].mindims.y = window_dims["height"]
+    keys["hint"].maxdims.y = window_dims["height"]
+
+    global lives_node
+    lives_node = keys["label"]
+    lives_node.document.text = "Remaining lives: "+str(player["lives"])
+
+    # load the spritesheet
+    global sprites
     sprites = load_spritesheet("pat_asteroids.json")
 
-    def get_texture_and_vertices(key):
-        img = sprites[key].frames[0].image
-        texture = img.get_texture()
-        w,h = img.width, img.height
-        vertices = -w/2,-h/2,0,w/2,-h/2,0,w/2,h/2,0,-w/2,h/2,0
-        vertex_list = pyglet.graphics.vertex_list_indexed(4,
-            [0, 1, 2, 0, 2, 3], ('v3f', vertices), ('t3f', texture.tex_coords))
-        return texture, vertex_list
+    # attach the listeners
+    w.launch_listener(frame)
+    w.launch_listener(keystate)
+    w.launch_listener(player_animation)
+    w.launch_listener(draw)
 
-    alive_player_texture, alive_player_vertices = get_texture_and_vertices("player")
-    dead_player_texture, dead_player_vertices = get_texture_and_vertices("dead-player")
-    asteroid_texture, asteroid_vertices = get_texture_and_vertices("asteroid")
+    pyglet.app.run()
+
+
+def player_animation():
+    global player
+    global player_display
+    global sprites
+
+    while True:
+        _, dt = yield "on_frame"
+
+        if player["alive"]:
+            # alive player: loop the player idle animation
+
+            # reset animation if needed
+            if player_display["key"] != "player":
+                player_display["frame"] = 0
+                player_display["frame_clock"] = 0
+                player_display["should_draw"] = True
+                player_display["key"] = "player"
+
+            # tick animation
+            player_display["frame_clock"] += dt
+
+            if player_display["frame_clock"] > sprites["player"].frames[player_display["frame"]].duration:
+                player_display["frame_clock"] -= sprites["player"].frames[player_display["frame"]].duration
+                player_display["frame"] += 1
+
+            # loop animation
+            if player_display["frame"] >= len(sprites["player"].frames):
+                player_display["frame"] = 0
+
+        else:
+            # dead player: play explosion animation once
+
+            # reset animation if needed
+            if player_display["key"] != "dead-player":
+                player_display["frame"] = 0
+                player_display["frame_clock"] = 0
+                player_display["should_draw"] = True
+                player_display["key"] = "dead-player"
+
+            if player_display["frame"] >= len(sprites["dead-player"].frames):
+                continue # animation has ended
+
+            # tick animation
+            player_display["frame_clock"] += dt
+
+            if player_display["frame_clock"] > sprites["dead-player"].frames[player_display["frame"]].duration:
+                player_display["frame_clock"] -= sprites["dead-player"].frames[player_display["frame"]].duration
+                player_display["frame"] += 1
+
+            if player_display["frame"] >= len(sprites["dead-player"].frames):
+                 # animation has ended, disable drawing
+                player_display["should_draw"] = False
+
+def draw():
+
+    global sprites
+
+    # modify the spritesheet a bit:
+    # anchor the images at their center. This informs the image.blit function.
+    for key in sprites.keys():
+        for frame in sprites[key].frames:
+            frame.image.anchor_x = frame.image.width/2
+            frame.image.anchor_y = frame.image.height/2
+
+    # pre_compute asteroid data
+    img = sprites["asteroid"].frames[0].image
+    w,h = img.width, img.height
+    vertices = -w/2,-h/2,0,w/2,-h/2,0,w/2,h/2,0,-w/2,h/2,0
+
+    # creating a vertex_list pre-loads the vertices onto the GPU
+    asteroid_texture = img.get_texture()
+    asteroid_vertex_list = pyglet.graphics.vertex_list_indexed(4,
+            [0, 1, 2, 0, 2, 3], ('v3f', vertices), ('t3f', asteroid_texture.tex_coords))
+
+    # cleanup
+    del img,w,h,vertices
 
     global player
     global asteroids
@@ -35,26 +150,17 @@ def draw(w):
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
+        # draw player via image.blit
+        if player_display["should_draw"]:
+            sprites[player_display["key"]].frames[player_display["frame"]].image.blit(player["x"],player["y"])
 
-        # draw player
-        if player["alive"]:
-            player_texture, player_vertices = alive_player_texture, alive_player_vertices
-        else:
-            player_texture, player_vertices = dead_player_texture, dead_player_vertices
+        # https://github.com/pyglet/pyglet/blob/master/pyglet/image/__init__.py
+        # line 1596 defines what actually happens when a texture is blit
+        # a little bit inefficient if you are drawing the same texture lots of times
 
-        glEnable(player_texture.target)
-        glBindTexture(player_texture.target, player_texture.id)
-        glPushAttrib(GL_COLOR_BUFFER_BIT)
+        # draw asteroids using a method built for drawing the same thing lots of times
 
-        glPushMatrix()
-        glTranslatef(player["x"],player["y"],0)
-        player_vertices.draw(GL_TRIANGLES)
-        glPopMatrix()
-
-        glPopAttrib()
-        glDisable(player_texture.target)
-
-        # draw asteroids
+        # asteroid texture only needs to be enabled once
         glEnable(asteroid_texture.target)
         glBindTexture(asteroid_texture.target, asteroid_texture.id)
         glPushAttrib(GL_COLOR_BUFFER_BIT)
@@ -62,16 +168,23 @@ def draw(w):
         for asteroid in asteroids:
 
             glPushMatrix()
+
+            # specify position via translation
             glTranslatef(asteroid["x"],asteroid["y"],0)
-            asteroid_vertices.draw(GL_TRIANGLES)
+
+            # vertex array is already stored on GPU. just draw those.
+            asteroid_vertex_list.draw(GL_TRIANGLES)
+
             glPopMatrix()
 
+        # asteroid texture needs to only be disabled once
         glPopAttrib()
         glDisable(asteroid_texture.target)
 
 
-@listener
-def keystate(w):
+def keystate():
+    # records key state in the wasd_keys dictionary
+
     global wasd_keys
     wasd_keys = {key:False for key in ["W","A","S","D"]}
 
@@ -85,8 +198,7 @@ def keystate(w):
 
 
 
-@listener
-def frame(w):
+def frame():
 
     global asteroids
     global window_dims
@@ -116,7 +228,7 @@ def frame(w):
             asteroids.remove(deleted_asteroid)
 
         # put more asteroids on screen
-        while len(asteroids) < 10:
+        while len(asteroids) < 20:
 
             new_asteroid = {
                     "x": random.random()*window_dims["width"],
@@ -128,7 +240,6 @@ def frame(w):
             asteroids.append(new_asteroid)
 
         if player["alive"]:
-
             #  move player
             v = 200
             dx,dy = 0,0
@@ -146,9 +257,12 @@ def frame(w):
 
                 r = 50
                 if dx*dx + dy*dy < r*r:
+                    # player is hit
+
                     player["alive"] = False
                     player["respawn_timer"] = 3
                     player["lives"] -= 1
+
                     lives_node.document.text = "Remaining lives: "+str(player["lives"])
                     if player["lives"] > 0: lives_node.document.text += ". Respawning..."
                     else:
@@ -156,15 +270,15 @@ def frame(w):
 
                     break
 
-        elif player["lives"] > 0:
+        elif player["lives"] > 0: # player is dead but has remaining lives
+
             player["respawn_timer"] -= dt
 
             if player["respawn_timer"] < 0:
-
+                # try to respawn player at this location:
                 x,y = window_dims["width"]/2,  100
 
                 no_asteroids_nearby = True
-
                 for asteroid in asteroids:
                     dx = asteroid["x"] - x
                     dy = asteroid["y"] - y
@@ -174,6 +288,7 @@ def frame(w):
                         no_asteroids_nearby = False
                         break
 
+                # only spawn player if that spot is free of asteroids
                 if no_asteroids_nearby:
                     player["alive"] = True
                     player["x"] = x
@@ -182,43 +297,6 @@ def frame(w):
                     lives_node.document.text = "Remaining lives: "+str(player["lives"])
 
 
-def run_game():
-
-
-    w = NodeWindow()
-    w.fps = 30 # yield "on_draw" and "on_frame" events 30 times per second
-
-    # text business
-    w.node, keys = deserialize_node("""
-    hint HintedLayoutNode
-    -PaddingLayoutNode [5,"*","*","*"]
-    --label LabelNode "{color (255,255,255,255)} placeholder"
-    """)
-
-    global window_dims
-    window_dims = {"width": 800, "height":600}
-    keys["hint"].mindims.x = window_dims["width"]
-    keys["hint"].maxdims.x = window_dims["width"]
-    keys["hint"].mindims.y = window_dims["height"]
-    keys["hint"].maxdims.y = window_dims["height"]
-
-    global player
-    player = {"x":100, "y":100, "alive":True, "respawn_timer": 0, "lives":3}
-
-    global lives_node
-    lives_node = keys["label"]
-    lives_node.document.text = "Remaining lives: "+str(player["lives"])
-
-    global asteroids
-    asteroids = [{"x":50, "y":150, "vx":100, "vy":100}]
-
-    frame(w)
-
-    keystate(w)
-
-    draw(w)
-
-    pyglet.app.run()
 
 if __name__ == "__main__":
     run_game()
--- spritesheet_editor/main.py
+++ spritesheet_editor/main.py
@@ -1,6 +1,20 @@
 
 from spritesheet_editor import SpritesheetEditorWindow
 import pyglet
+import sys, os
 
-w = SpritesheetEditorWindow()
+if len(sys.argv) == 1:
+    SpritesheetEditorWindow()
+else:
+    for f in sys.argv[1:]:
+        if not os.path.isfile(f):
+            print("No such file:", f)
+            break
+        if os.path.splitext(f)[1] not in [".json",".png"]:
+            print("File ", f, " is not a .json or .png")
+            break
+        SpritesheetEditorWindow(fname=f)
+
+
+
 pyglet.app.run()
