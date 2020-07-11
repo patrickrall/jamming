db9fc49f688edf5e108579247beb7070276113ff
Patrick Rall <patrickrall.ddns.net>
2020-07-10 20:04:44 -0500

    proto

--- keyboardtennis/proto.py
+++ keyboardtennis/proto.py
@@ -1,6 +1,13 @@
 
+import json
+from swyne.node import NodeWindow,Vector2,deserialize_node
+from swyne.layout import HintedLayoutNode
+import pyglet
+from pyglet.gl import *
+
 def main():
 
+    global keys
     keys = ["`", "1", "2","3","4","5","6","7","8","9", "0", "-", "=", "del",
             "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\",
             "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter",
@@ -9,43 +16,68 @@ def main():
     # make window
 
     global keys_pressed
-    keys_pressed = {
-        "`": False,
-        "1": True,
-        ...
-        }
+    keys_pressed = {key:False for key in keys}
+
+
+    with open("keyboard.json") as f:
+        data = json.loads(f.read())
 
     global key_rects
-    key_rects = {
-        "`": {"x":0, "y":0  "w", 10, "h": 10},
-        "1": {"x":0, "y":0  "w", 10, "h": 10},
-        "2": {"x":0, "y":0  "w", 10, "h": 10},
-        "3": {"x":0, "y":0  "w", 10, "h": 10},
-    }
+    key_rects = {keys[i]:data["frames"][str(i)]["frame"] for i in range(len(keys))}
 
-    global balls
-    balls = [{"pos":Vector2, "vel": Vector2}]
+    #global balls
+    #balls = [{"pos":Vector2, "vel": Vector2}]
 
     global level
     level = {
         "default": ["none","wall"],
-        "~": ["wall", "none"],
-        "y": ["none", "mud"],
+        "`": ["wall", "none"],
+        "y": ["none", "goal"],
         "k": ["goal"],
+        "c": ["hazard"],
+        "lshift": ["hazard"],
     }
+
+    w = NodeWindow()
+    w.fps = 30
+
+    w.node, _ = deserialize_node("""
+    HintedLayoutNode [960,230]
+    """)
+
+    # Stella's numbers are: [960,230]
+
+    w.launch_listener(draw)
+    pyglet.app.run()
 
 
 
 def draw():
+
+    colors = {
+        "none": [1.,1.,1.,1.],
+        "wall": [0.,0.,0.,1.],
+        "hazard": [1.,0.,0.,1.],
+        "goal": [1.,1.,0.,1.],
+    }
 
     while True:
         yield "on_draw"
 
         # draw balls
 
-        # draw key as rectangle
-        glColor4f(0.3,0.5,0.2,0.9)
-        glRectf(x1,y1,x2,y2)
+        for key in keys:
+            idx = 1 if keys_pressed[key] else 0
+
+            if key in level: value = level[key][idx]
+            else: value = level["default"][idx]
+
+            color = colors[value]
+
+            # draw key as rectangle
+            glColor4f(color[0],color[1],color[2],color[3])
+            rect = key_rects[key]
+            glRectf(rect["x"],rect["y"],rect["x"]+rect["w"],rect["y"]+rect["h"])
 
 
 # types of things a key can be
@@ -58,4 +90,5 @@ def draw():
 #  - gravity?
 
 
-
+if __name__ == "__main__":
+    main()
