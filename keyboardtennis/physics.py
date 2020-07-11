2a47ed460b1e3486a50da8edb376fcfb1b6db895
cousday <cousday@gmail.com>
2020-07-11 12:31:00 -0400

    ok now the bugs are actually fixed

--- keyboardtennis/physics.py
+++ keyboardtennis/physics.py
@@ -44,11 +44,11 @@ def ball_spawning():
                     bl_corner = ball_spawner["bot_left"]
                     # calculate speed and angle of velocity, and position
                     sp = vp[3] * (vp[0] % vp[4]) + vp[2]
-                    th = ((vp[0] % vp[7]) + vp[5]) * (math.pi / (2*vp[6]))
+                    th = ((vp[0] % vp[7]) + vp[5]) * (pi / (2*vp[6]))
                     pos = [bl_corner[0] + vp[1], bl_corner[1] + vp[1]]
                     # add new ball!
                     balls.append({"pos":Vector2(pos[0], pos[1]), \
-                        "vel": Vector2(sp*math.cos(th), sp*math.sin(th)),\
+                        "vel": Vector2(sp*cos(th), sp*sin(th)),\
                         "caught": "none", "dia":vp[1], "extratime":0})
                     # reset control key counter
                     ball_spawner["ctrl_held"] = False
@@ -163,6 +163,8 @@ def frame():
         _, dt = yield "on_frame"
 
 
+
+
         for ball in balls:
             dimsx,dimsy,boty = 960,320,60
 
@@ -193,38 +195,37 @@ def frame():
                 pos += nudge
 
                 # check for collision between ball, a wall
-                for ball in balls:  # check each ball
-                    for key in keys:  # check each key to see if the key is currently at a wall
-                        hitWall = False
-                        # specific key wall check
-                        if key in level and key in keys_pressed:
-                            # non-default wall check
-                            if (level[key][len(level[key]) - 1] if keys_pressed[key] else level[key][0]) == "wall":  # check for wall
-                                hitWall = True
-                        # default-key wall check
-                        elif (level["default"][1] if keys_pressed[key] else level["default"][0] == "wall"):
+                for key in keys:  # check each key to see if the key is currently at a wall
+                    hitWall = False
+                    # specific key wall check
+                    if key in level and key in keys_pressed:
+                        # non-default wall check
+                        if (level[key][len(level[key]) - 1] if keys_pressed[key] else level[key][0]) == "wall":  # check for wall
                             hitWall = True
+                    # default-key wall check
+                    elif (level["default"][1] if keys_pressed[key] else level["default"][0] == "wall"):
+                        hitWall = True
 
-                        if hitWall:
-                            # change velocity in response to the wall hit
-                            # print(key + " is a wall")
-                            rect = key_rects[key]
-                            key_pos = Vector2(rect["x"], rect["y"])
-                            key_size = Vector2(rect["w"], rect["h"])
-                            if ball_rect_can_collide(ball["pos"], ball["dia"] / 2, key_pos, key_size):
-                                #print("Hit " + key)
-                                rect_center = Vector2(key_pos.x + key_size.x/2, key_pos.y + key_size.y/2)
-                                [sign_x, sign_y, dx, dy] = collision_sign(ball["pos"], rect_center, ball[
-                                    "dia"] / 2 + key_size.x / 2 )  # todo account for key_size.y separately
-                                #print(f"[sign_x, sign_y, dx, dy] = [{sign_x}, {sign_y}, {dx}, {dy}]")
+                    if hitWall:
+                        # change velocity in response to the wall hit
+                        # print(key + " is a wall")
+                        rect = key_rects[key]
+                        key_pos = Vector2(rect["x"], rect["y"])
+                        key_size = Vector2(rect["w"], rect["h"])
+                        if ball_rect_can_collide(ball["pos"], ball["dia"] / 2, key_pos, key_size):
+                            #print("Hit " + key)
+                            rect_center = Vector2(key_pos.x + key_size.x/2, key_pos.y + key_size.y/2)
+                            [sign_x, sign_y, dx, dy] = collision_sign(ball["pos"], rect_center, ball[
+                                "dia"] / 2 + key_size.x / 2 )  # todo account for key_size.y separately
+                            #print(f"[sign_x, sign_y, dx, dy] = [{sign_x}, {sign_y}, {dx}, {dy}]")
 
-                                if (sign_x != 0):
-                                    ball["vel"].x = sign_x * abs(ball["vel"].x)
-                                    #print("x vel adjust")
-                                elif (sign_y != 0):
-                                    ball["vel"].y = sign_y * abs(ball["vel"].y)
-                                    #print("y vel adjust")
-                                break
+                            if (sign_x != 0):
+                                ball["vel"].x = sign_x * abs(ball["vel"].x)
+                                #print("x vel adjust")
+                            elif (sign_y != 0):
+                                ball["vel"].y = sign_y * abs(ball["vel"].y)
+                                #print("y vel adjust")
+                            break
             ball["pos"] = pos
 
 
