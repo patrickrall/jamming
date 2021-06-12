4c823588b82933f599ec4a01daa5a7e45f0fe0c1
firelighted <asano.sar@gmail.com>
2021-06-12 11:11:43 +0500

    color blocks change together and the flood-fill color cycle

--- 2021/polygon_drawing/edit_triangles.py
+++ 2021/polygon_drawing/edit_triangles.py
@@ -44,19 +44,54 @@ def grid_editor_loop():
                     poly_p1, poly_p2, poly_p3 = globs.polygons[poly_i].get_points()
                     print("polygon at index=", poly_i, " at points ", poly_p1, poly_p2, poly_p3)
                     if poly_i > -1:
-                        globs.polygons[poly_i].set_color(globs.colors[globs.colorIndex])
+                        globs.tri["updatedNeighbors"] = [] # reset color change tally
+                        change_polygon_color_cascade(poly_i)
+                        increment_color_index()
 
+def change_polygon_color_cascade(poly_i):
+    globs.tri["updatedNeighbors"].append(poly_i) # mark this triangle as updated
+    oldColor = globs.polygons[poly_i].get_color()
+    newColor = globs.colors[globs.colorIndex]
+    # update the clicked triangle
+    globs.polygons[poly_i].set_color(newColor)
+    points = globs.polygons[poly_i].get_points()
+    neighbors_i = get_neighbors_from_centroid(points[0], points[1], points[2])
+    print(neighbors_i)
+    for k in neighbors_i:
+        if k == poly_i or k in globs.tri["updatedNeighbors"]:
+            continue # skip triangles that have already been updated
+        neighborColor = globs.polygons[k].get_color()
+        if (neighborColor.x != oldColor.x or neighborColor.y != oldColor.y or neighborColor.z != oldColor.z):
+            continue
+        else:
+            change_polygon_color_cascade(k)
+            print(k)
+
+def increment_color_index():
+    globs.colorIndex += 1
+    if globs.colorIndex >= len(globs.colors):
+        globs.colorIndex = 0
 
 def get_polygon_index(p1, p2, p3, isUpper):
     for k in range(0, globs.tri["i_max"] * globs.tri["j_max"] * 2):
         points = globs.polygons[k].get_points()
         diffX = p1.x - points[0].x + p2.x - points[1].x + p3.x - points[2].x
         diffY = p1.y - points[0].y + p2.y - points[1].y + p3.y - points[2].y
-        if diffX < .1 and diffY < .1:
+        if abs(diffX) < .1 and abs(diffY) < .1:
             return k
     print("No polygon found")
     return -1
 
+def get_neighbors_from_centroid(p1, p2, p3):
+    limit = globs.tri["scaleX"] * 2
+    neighbors = []
+    centroid = Vec((p1.x + p2.x + p3.x)/3, (p1.y + p2.y + p3.y)/3)
+    for k in range(0, len(globs.polygons)):
+        points = globs.polygons[k].get_points()
+        n_centroid = Vec((points[0].x + points[1].x + points[2].x)/3, (points[0].y + points[1].y + points[2].y)/3)
+        if abs(centroid.x - n_centroid.x) + abs(centroid.y - n_centroid.y) < limit:
+            neighbors.append(k)
+    return neighbors
 
 def sign ( p1,  p2,  p3):
     return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y)
